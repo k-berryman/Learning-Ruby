67606956d564a13c2441234f328c83460f16fa35
Kaitlin Berryman <k.berrygirl.10@gmail.com>
2020-10-18 10:48:39 -0400

    Create Intro.rb

---
+++ Intro.rb
@@ -0,0 +1,45 @@
+# print
+print "I don't need parentheses or semicolons??"
+
+# puts adds blank line after printing
+puts "Woah"
+puts "I puts"
+
+# datatypes are number, boolean, string
+my_num = 23 % 7;
+
+# everything in Ruby is an object
+# methods are tied to objects
+
+puts "I dont need ANY ( )??".length
+
+# lol
+puts "kayak".reverse
+puts "tacocat".reverse
+
+puts "Kaitlin".reverse
+
+puts "im so excited".upcase
+puts "OKAY I'M GOOD NOW".downcase
+
+=begin
+I'm a multi-line comment!!
+This is actually really readable!!
+Don't put spaces between = and begin/end
+=end
+
+naming_convention = "underscores"
+
+puts sum = 10 + 300
+puts product = 900 * 2
+puts quotient = 20 / 5
+
+name = "Kaitlin"
+
+puts name.downcase   # kaitlin
+puts name.reverse    # niltiaK
+puts name.upcase     # KAITLIN
+puts name            # Kaitlin
+
+name = "Berryman"
+puts name.downcase.reverse.upcase   # NAMYRREB
